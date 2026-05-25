f1dbdbcf2eb62dad5bf6a076fbb91fc9401b97da
Tetsuya-hahaha <Tetsu.030609@gmail.com>
2026-05-25 18:17:57 +0900

    Delete HFL_AP_selection-main directory

--- HFL_AP_selection-main/result.py
+++
@@ -1,9 +0,0 @@
-class simResult:
-    satisHarmeanArray: list 
-    termNumOverLimitArray: list
-    linkTerm: list
-
-    def __init__(self) :
-        self.satisHarmeanArray = []
-        self.termNumOverLimitArray = []
-        self.linkTerm = []
